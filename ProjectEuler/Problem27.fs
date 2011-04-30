d3d2809ac2694e9117a65240f1de388f9208d201
Alex Ullrich <alex.ullrich@gmail.com>
2011-04-30 14:59:39 -0400

    naive, slow solution for #27

---
+++ ProjectEuler/Problem27.fs
@@ -0,0 +1,30 @@
+﻿module ProjectEuler.Problem27
+
+#light
+
+let solve =
+    let primes100k = 
+        PrimeFinder.primes 100000L //should be enough
+        |> Seq.map (fun x -> int x)
+        |> List.ofSeq
+       
+    let isPrime x =
+        primes100k |>
+        Seq.exists((=) x)
+
+    let mutable max = 0
+    let mutable result = 0
+
+    for a in -1000..1..1000 do
+        for b in -1000..1..1000 do
+            let mutable seqLength = 0
+            let mutable num = 0
+            while num = 0 || isPrime num do 
+                num <- seqLength*seqLength + a*seqLength + b
+                seqLength <- seqLength + 1
+            
+            if seqLength > max then
+                max <- seqLength
+                result <- a * b
+
+    result
