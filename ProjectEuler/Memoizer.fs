b7c3a69522a24047e81d16c4b2a94bcd91d2ea0e
Alex Ullrich <alex.ullrich@gmail.com>
2011-04-30 15:50:46 -0400

    some optimizations to #27 (still iterative)   - memoize checks for prime numbers   - only lookup against primes < 1000   - only loop over odd numbers

---
+++ ProjectEuler/Memoizer.fs
@@ -0,0 +1,14 @@
+﻿module ProjectEuler.Memoizer
+
+#light
+open System.Collections.Generic
+
+let memoize f =
+    let cache = Dictionary<_, _>()
+    fun x ->
+        let found, res = cache.TryGetValue(x)
+        if found then res
+        else
+            let res = f x
+            cache.[x] <- res
+            res
--- ProjectEuler/Problem27.fs
+++ ProjectEuler/Problem27.fs
@@ -3,20 +3,21 @@
 #light
 
 let solve =
-    let primes100k = 
-        PrimeFinder.primes 100000L //should be enough
-        |> Seq.map (fun x -> int x)
+    let primes1k = 
+        PrimeFinder.primes 1000L //should be enough
+        |> Seq.map (int)
         |> List.ofSeq
-       
-    let isPrime x =
-        primes100k |>
-        Seq.exists((=) x)
+    
+    let isPrime = 
+        let isPrimeFunc num =
+            primes1k |> List.exists((=) num)
+        Memoizer.memoize isPrimeFunc
 
     let mutable max = 0
     let mutable result = 0
 
-    for a in -1000..1..1000 do
-        for b in -1000..1..1000 do
+    for a in -999..2..999 do
+        for b in -999..2..999 do
             let mutable seqLength = 0
             let mutable num = 0
             while num = 0 || isPrime num do 
