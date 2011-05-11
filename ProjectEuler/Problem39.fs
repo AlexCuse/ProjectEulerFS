867f58705e68cafcee467b9a3926dfc52f1dd72f
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-10 20:53:55 -0400

    Still trying to figure out best way to format this code - this seesm to aid readability the most for now

--- ProjectEuler/Problem39.fs
+++ ProjectEuler/Problem39.fs
@@ -14,14 +14,13 @@ let possibleTriangles perimeter =
     // however for all sides of any triangle, c < (p / 2)
     // (p / 3) < c < (p / 2)
     [(3 * perimeter)/10 .. (5 * perimeter)/10] //only need integers
-    |> List.filter (fun c ->
-        [1..perimeter]
-        |> Seq.takeWhile (fun x -> (x + c) < perimeter) //highest value that has remote possibility of matching 
-        |> Seq.exists (fun b -> (pown (perimeter-b-c) 2 + pown b 2) = pown c 2) // aa + bb == cc
-    ) 
-    |> List.length
+        |> List.filter (fun c ->
+            [1..perimeter]
+                |> Seq.takeWhile (fun x -> (x + c) < perimeter) //highest value that has remote possibility of matching 
+                |> Seq.exists (fun b -> (pown (perimeter-b-c) 2 + pown b 2) = pown c 2)) // aa + bb == cc
+        |> List.length
 
 let solve = 
     [1..1000] 
-    |> List.sortBy ( fun x -> -1 * possibleTriangles x)
-    |> List.head
+        |> List.sortBy ( fun x -> -1 * possibleTriangles x)
+        |> List.head
