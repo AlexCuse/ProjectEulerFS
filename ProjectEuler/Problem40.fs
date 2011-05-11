0485150885e907c88c0314bc8134ce7d22ab0e00
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-10 21:43:22 -0400

    Solution for #40

---
+++ ProjectEuler/Problem40.fs
@@ -0,0 +1,18 @@
+﻿module ProjectEuler.Problem40
+
+#light
+
+open Util
+
+let strangeDigits =
+    Seq.unfold (fun x -> Some(x, x + 1)) 1 //all positive integers
+    |> Seq.collect (fun num -> (num |> digitsFrom)) //all digits of each
+
+let digit n =
+    strangeDigits
+    |> Seq.nth (n - 1)
+
+let solve =
+    [0..6] 
+        |> List.map (fun n -> digit (pown 10 n)) 
+        |> List.reduce (fun acc x -> acc * x)  //could use fold here, but this may describe operation better
