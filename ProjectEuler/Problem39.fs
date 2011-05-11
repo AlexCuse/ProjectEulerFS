cc21301a252012cd5db8de4445721e55b1c5fa1b
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-10 20:49:29 -0400

    Adding Solution to #39 (very brute force, takes ~7 seconds)

--- ProjectEuler/Problem39.fs
+++ ProjectEuler/Problem39.fs
@@ -4,16 +4,19 @@
 
 // can pythagorean triplets (only getting 2 vals but can be extended) in p9 help for this?
 
-// a * a + b * b = c * c
 // a + b + c = p
-// p < 1000
-// a < b < c ?
+// a * a + b * b = c * c
+// a <= b < c
 
 let possibleTriangles perimeter =
-    [(4 * perimeter)/10 .. (6 * perimeter)/10]
+    // any side of an equilateral triangle will = p/3
+    // c will always be opposite largest angle - longest leg, so  c > (p/3)
+    // however for all sides of any triangle, c < (p / 2)
+    // (p / 3) < c < (p / 2)
+    [(3 * perimeter)/10 .. (5 * perimeter)/10] //only need integers
     |> List.filter (fun c ->
         [1..perimeter]
-        |> Seq.takeWhile (fun x -> (x + c) < perimeter) // check could be smarter, but this rules out extreme impossibilities
+        |> Seq.takeWhile (fun x -> (x + c) < perimeter) //highest value that has remote possibility of matching 
         |> Seq.exists (fun b -> (pown (perimeter-b-c) 2 + pown b 2) = pown c 2)) // aa + bb == cc
     |> List.length
 
