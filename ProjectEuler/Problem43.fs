f538a147378ff6ffa3e551686fa6c6a17bf94902
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-11 13:11:24 -0400

    Slight refactoring to #43 for readability

--- ProjectEuler/Problem43.fs
+++ ProjectEuler/Problem43.fs
@@ -5,16 +5,23 @@
 let toNumber digits = //as long
     digits |> Seq.fold (fun acc x -> acc * 10L + x) 0L
 
-let curious digits =
-    let sevenPrimes = 
-        Seq.unfold (fun n -> Some(n, n+1)) 1
-        |> Seq.filter Util.isPrime
-        |> Seq.take 7
-        |> Array.ofSeq
+let sevenPrimes =
+    Seq.unfold (fun n -> Some(n, n+1)) 1
+    |> Seq.filter Util.isPrime
+    |> Seq.take 7
+    |> Array.ofSeq
 
+(*
+sequence within digits ([2..4]..[8..10])
+convert digit subsets to number
+first number should be divisible by first prime, second by second .. seventh by seventh
+*)
+let curious digits =  
     let arr = digits |> Array.ofList
-    [1..digits.Length - 3]
-    |> List.forall (fun x -> (arr.[x..x+2] |> toNumber) % (int64 sevenPrimes.[x-1]) = 0L)   
+    let check x =
+        (arr.[x..x+2] |> toNumber) % (int64 sevenPrimes.[x-1]) = 0L
+    
+    [1..digits.Length - 3]|> List.forall check   
 
 let solve =
     Util.permutations [0L..9L] Set.empty
