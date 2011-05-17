b52956b0ef1216fb68b17492b19d6c5926edf108
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-17 08:09:37 -0400

    Solution for #46

---
+++ ProjectEuler/Problem46.fs
@@ -0,0 +1,24 @@
+﻿module ProjectEuler.Problem46
+
+#light
+
+open Util
+
+let oddComposites =
+    Seq.unfold (fun x -> Some(x, x + 2)) 3
+    |> Seq.filter (fun x -> not(isPrime x))
+
+let primes =
+    PrimeFinder.primes 1000000L
+    |> List.map int
+
+let goldbach n =
+    primes 
+    |> Seq.takeWhile(fun x -> x < n)
+    |> Seq.exists (fun x -> sqrt(double((n - x) / 2)) % 1.0 = 0.0)
+
+let solve =
+    oddComposites
+    |> Seq.filter (fun n -> not(goldbach n))
+    |> Seq.head
+
