db1c512dea4643a7eba7ee44b4a91094cd95a7c3
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-19 13:22:24 -0400

    Problem 48 solution

---
+++ ProjectEuler/Problem48.fs
@@ -0,0 +1,13 @@
+﻿module ProjectEuler.Problem48
+
+#light
+
+open System.Numerics
+
+let solve =
+    Seq.unfold (fun n -> Some(n, n + 1I)) 1I
+    |> Seq.take 1000
+    |> Seq.map (fun n -> pown n (int n))
+    |> Seq.sum
+    |> string
+    |> fun str -> str.Substring(str.Length - 10)
