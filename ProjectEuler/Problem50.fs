02eebf9b2d1691e3bb6fc04085032a022724780a
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-23 13:43:40 -0400

    Problem 50 solution

---
+++ ProjectEuler/Problem50.fs
@@ -0,0 +1,19 @@
+﻿module ProjectEuler.Problem50
+
+#light
+
+let primes =
+    PrimeFinder.primes 999999L
+    |> List.map int
+    |> Seq.ofList
+
+let primeSeq n =
+    primes 
+    |> Seq.filter (fun x -> x > n)
+    |> Seq.scan (fun (sum, count) x -> (sum+x, count+1)) (n, 1)
+    |> Seq.takeWhile (fun (sum, count) -> sum < 1000000)
+    |> Seq.filter (fun (sum, count) -> Util.isPrime sum)
+    |> Seq.maxBy (fun (sum, count) -> count)
+
+let solve =
+    primes |> Seq.map primeSeq |> Seq.maxBy (fun (sum, count) -> count)
