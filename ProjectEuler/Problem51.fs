91f160a9d9d602b121862035a8f80be42f9c4df2
Alex Ullrich <alex.ullrich@gmail.com>
2011-05-24 14:03:36 -0400

    Problem 51 & 52

---
+++ ProjectEuler/Problem51.fs
@@ -0,0 +1,47 @@
+﻿module ProjectEuler.Problem51
+
+#light
+
+open Util
+
+let primes = PrimeFinder.primes 200000L |> List.map int
+
+let rec digitCombinations x digits =
+    match x, digits with
+    | 0, _ -> [[]]
+    | _, [] -> []
+    | z, (v::w) -> List.map ((@) [v]) (digitCombinations (z-1) w) @ (digitCombinations z w)
+
+    //this doesn't work, seeming equivalent above does
+    //| z, (v::w) -> (digitCombinations (z-1) w) @ (digitCombinations z w) |> List.map (fun item -> [v] @ item)
+
+let replacements numDigits = 
+    [1..numDigits - 1] |> List.collect (fun x -> digitCombinations x [0..numDigits-1])
+       
+let replaceDigits digits n =
+    let nDigits = n.ToString().ToCharArray()
+    [0..9]
+    |> List.map (fun x ->
+        List.init nDigits.Length (fun y ->
+            if List.exists (fun z -> z = y) digits
+            then x.ToString()
+            else nDigits.[y].ToString())
+        |> List.reduce (+))
+    |> List.map int
+                
+let primeReplacements n len =
+    replacements (n |> digitsFrom |> List.length)
+        |> List.map (fun x ->
+            replaceDigits x n
+            |> List.filter (fun y -> (n |> digitsFrom |> List.length) = (y |> digitsFrom |> List.length) && Util.isPrime y))
+        |> List.filter (fun lst -> lst.Length >= len)
+     
+let solve =
+    primes 
+        |> Seq.skipWhile (fun n -> n <= 56003)
+        |> Seq.map (fun n -> primeReplacements n 8)
+        |> Seq.filter (fun l -> l.Length > 0)
+        |> Seq.head
+        |> List.map (fun lst -> List.min lst)
+        |> Seq.min
+
---
+++ ProjectEuler/Problem52.fs
@@ -0,0 +1,18 @@
+﻿module ProjectEuler.Problem52
+
+#light
+
+open Util
+
+let sameDigits x =
+    [2..6]
+        |> List.map (fun z -> z * x)
+        |> List.map digitsFrom
+        |> List.map List.sort
+        |> Seq.distinct
+        |> Seq.length = 1
+
+let solve =
+    Seq.unfold (fun x -> Some(x , x + 1)) 1
+        |> Seq.filter sameDigits
+        |> Seq.head
