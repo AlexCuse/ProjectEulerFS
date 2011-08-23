393ee253aa2092a583d9b374fcc32d1324b1d6fb
Alex Ullrich <alex.ullrich@gmail.com>
2011-08-23 11:30:56 -0400

    (Cheating) solution to problem 97

---
+++ ProjectEuler/Problem97.fs
@@ -0,0 +1,16 @@
+﻿module ProjectEuler.Problem97
+
+#light
+
+let bpow = System.Numerics.BigInteger.Pow
+
+let lastTenDigitsFrom n =
+    let rec decomposeFunc n listSoFar =
+        if n = 0I || ((listSoFar |> List.length) = 10) then listSoFar
+        else decomposeFunc (n / 10I) ([n % 10I] @ listSoFar)
+    decomposeFunc n []
+
+let solve =
+    let num = 28433I * bpow(2I, 7830457) + 1I
+    lastTenDigitsFrom num
+    
