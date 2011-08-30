9e4fe3bdb8b70a2df2a672d27cd88e0160e688b8
Alex Ullrich <alex.ullrich@gmail.com>
2011-08-30 14:19:19 -0400

    Problem 71 solution

---
+++ ProjectEuler/Problem71.fs
@@ -0,0 +1,15 @@
+﻿module ProjectEuler.Problem71
+
+#light
+
+//http://en.wikipedia.org/wiki/Farey_sequence
+
+let solve = 
+    let mutable a, c = 2, 5
+    let mutable b, d = 3, 7
+
+    while (c + d) <= 1000000 do
+        a <- a + b
+        c <- c + d
+
+    a, c //numerator, denominator
